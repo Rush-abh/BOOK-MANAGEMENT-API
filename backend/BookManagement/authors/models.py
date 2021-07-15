4f87259eee27553a77efcbe718a0ff09b3ec378d
Rush-abh <rushabh988@gmail.com>
2021-07-15 19:39:45 +1000

    updated to author model to use defult id

--- backend/BookManagement/authors/models.py
+++ backend/BookManagement/authors/models.py
@@ -9,9 +9,8 @@ from django.db import models
 
 
 class Author(models.Model):
-    author_id = models.AutoField(primary_key=True, editable=False)
     first_name = models.TextField(max_length=50)
     last_name = models.TextField(max_length=50)
 
     def __str__(self):
-        return "%s %s %s" % (self.author_id, self.first_name, self.last_name)
+        return "%s %s" % (self.first_name, self.last_name)
