b3e47ca16d9ff6508f0b987b83f75dd3b8a1f2e0
Rush-abh <rushabh988@gmail.com>
2021-07-15 20:25:34 +1000

    Books serializer updated

--- backend/BookManagement/books/serializers.py
+++ backend/BookManagement/books/serializers.py
@@ -8,7 +8,7 @@ from books.models import Book
 
 
 # Default serializer with all fields of model
-class AuthorSerializer(serializers.ModelSerializer):
+class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = "__all__"
