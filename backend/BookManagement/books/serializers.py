1021573ee82f80d6411cf9770afa49889bb167e8
Rush-abh <rushabh988@gmail.com>
2021-07-15 23:21:23 +1000

    created nested serializer for book details

--- backend/BookManagement/books/serializers.py
+++ backend/BookManagement/books/serializers.py
@@ -3,12 +3,25 @@ Program to serialize of Books Details
 Edited By: Rushabh Pancholi
 """
 
+from re import A
+from django.db.models.expressions import F
 from rest_framework import serializers
 from books.models import Book
+from authors.models import Author
+from authors.serializers import AuthorSerializer
 
 
-# Default serializer with all fields of model
+# serializer with all fields of model
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = "__all__"
+
+
+# serializer using Nested relationships with Author,
+class BookDetailSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Book
+        fields = ['name', 'isbn', 'author']
