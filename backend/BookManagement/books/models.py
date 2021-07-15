31cf9b30d3aea05ba4664645171d7c199da8796a
Rush-abh <rushabh988@gmail.com>
2021-07-15 23:19:01 +1000

    removed pk, updated author field

--- backend/BookManagement/books/models.py
+++ backend/BookManagement/books/models.py
@@ -12,13 +12,8 @@ from authors.models import Author
 class Book(models.Model):
     name = models.TextField(max_length=256)
     isbn = models.TextField(max_length=13)
-    author = models.ForeignKey(Author, null=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(
+        Author, null=True, related_name='author', on_delete=models.SET_NULL)
 
     def __str__(self):
         return '%s %s %s' % (self.name, self.isbn, self.author)
-
-    def save(self, *args, **kwargs):
-        name = self.name
-        isbn = self.isbn
-        author = self.author
-        super(Book, self).save()
