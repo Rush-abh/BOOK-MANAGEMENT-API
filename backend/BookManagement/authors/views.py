48079aa0b12518359daa4a21c2f8faf264fc2edc
Rush-abh <rushabh988@gmail.com>
2021-07-15 19:40:57 +1000

    updated to author view for author detail voew

--- backend/BookManagement/authors/views.py
+++ backend/BookManagement/authors/views.py
@@ -2,6 +2,7 @@
 Program to process the data of Author Details
 Edited By: Rushabh Pancholi
 """
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from authors.models import Author
@@ -12,6 +13,7 @@ from rest_framework.parsers import JSONParser
 
 # function based view implementation
 # Handles GET and POST request
+@csrf_exempt
 @api_view(['GET', 'POST'])
 def authors_list(request):
 
@@ -28,3 +30,27 @@ def authors_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def author_detail_view(request, pk):
+
+    try:
+        author = Author.objects.get(pk=pk)
+    except Author.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = AuthorSerializer(author)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = AuthorSerializer(author, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        author.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
